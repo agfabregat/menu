bd858699c7f8a3e306c8d309fd858546d99ebb02
Adolfo Fabregat <agfabregat@hotmail.com>
2022-11-03 10:45:59 -0400

    prototypes

---
+++ js/prototypes.js
@@ -0,0 +1,44 @@
+[
+{"href":"https://www.flashscore.com/tennis/wta-singles/","title":"WTA Schedule"},
+{"href":"https://playarea.azurewebsites.net/papers.html","title":"Morning Papers"},
+{"href":"https://www.dailymail.co.uk/ushome/index.html","title":"Daily Mail US"},
+{"href":"https://www.the-sun.com/entertainment/","title":"The Sun Entertainment"},
+
+{"href":"https://1equals1.club","title":"Queries"},
+{"href":"https://agfservicecorsproxy20220326232156.azurewebsites.net/htmlget.html","title":"HtmlGet"},
+
+{"href":"https://www.livesporttv.com/","title":"SportsOnTv"},
+{"href":"https://www.fis-ski.com/DB/general/live.html","title":"Alpine Skiing"},
+
+{"href":"https://www.rocketlaunch.live/","title":"Rocket Launches"},
+{"href":"https://spaceflightnow.com/launch-schedule/","title":"Rocket Launches2"},
+{"href":"https://popminute.com/tv-tonight/","title":"TV Primetime"},
+
+{"href":"https://noises.online/player.php?g=ha5","title":"MyNoise"},
+{"href":"https://binance.us/en/trade/pro/XNO_USD","title":"XNO USD"},
+{"href":"http://agfabregat-001-site2.atempurl.com","title":"DNS test"},
+
+
+{"href":"http://clexplorer.azurewebsites.net/clexplorer.html","title":"clExplorer"},
+{"href":"http://agfabregat-001-site1.atempurl.com/","title":"Awesome Airfare Helper"},
+{"href":"http://agfabregat-001-site3.atempurl.com/rss-reader.html#","title":"rssReader"},
+
+{"href":"http://agfabregat-001-site3.atempurl.com/content/scitech.html","title":"Sci Tech"},
+
+
+{"href":"https://taskandpurpose.com/","title":"Task & Purpose"},
+{"href":"http://www.thedrive.com/","title":"The Drive"},
+
+{"href":"https://news.usni.org/","title":"US Naval Inst."},
+{"href":"https://nationalinterest.org/","title":"National Interest"},
+{"href":"https://www.strategypage.com/default.aspx","title":"Strategy Page"},
+{"href":"Nitrogen.html","title":"Nitrogen"},
+
+{"href":"http://oddsapi.azurewebsites.net/odds.html","title":"Odds"},
+{"href":"http://zjx.azurewebsites.net/zjx.html","title":"ZJX"},
+{"href":"MobileScreen.html","title":"Mobile Screen"},
+{"href":"ScreenWidths.html","title":"ScreenWidths"},
+{"href":"LineOriginators.html","title":"Line Originators"},
+{"href":"STICKYHEADERSFOOTERS.html","title":"StickyHeadersFooters"}
+
+]
