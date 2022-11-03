ca8b333336b0d5af86ccaf7fe629b30d4842f796
Adolfo Fabregat <agfabregat@hotmail.com>
2022-11-03 09:47:25 -0400

    blogs

---
+++ js/blogs.js
@@ -0,0 +1,88 @@
+<li><a href='http://www.firmaspress.com/'>Carlos Montaner</a></li>
+
+<li><a href='http://www.nationaljournal.com/'>  NatJournal/Hotline</a></li>
+<li><a href='https://patriotpost.us/opinion'>Patriot Post</a></li>
+<li><a href='http://powerlineblog.com/'>Powerlineblog</a></li>
+<li><a href='http://libaterian.blogspot.com/'>Libaterian</a></li>
+<li><a href='http://www.steynonline.com/'>Steyn online</a></li>
+<li><a href='http://theconservativetreehouse.com/'>Conservative TreeHouse</a></li>
+<li><a href='https://truepundit.com/'>TruePundit</a></li>
+<li><a href='https://thedesk.matthewkeys.net/'>TheDesk Matthew Keys</a></li>
+
+<li><a href='https://www.scottadamssays.com/'>Scott Adams</a></li>
+<li><a href='http://ace.mu.nu/'>Ace Of Spades</a></li>
+<li><a href='http://www.theamericanconservative.com/'>Am Conservative</a></li>
+<li><a href='https://amgreatness.com/'>Am Greatness</a></li>
+<li><a href='http://americanlookout.com/'>Am Lookout</a></li>
+<li><a href='https://bigleaguepolitics.com/'>Big League Politics</a></li>
+<li><a href='https://www.dailywire.com/'>Daily Wire</a></li>
+
+
+<li><a href='https://twitter.com/jamiedupree?lang=en'>Jamie Dupree Twitter</a></li>
+<li><a href='http://donsurber.blogspot.com/'>Don Surber</a></li>
+<li><a href='https://danieljmitchell.wordpress.com/'>Daniel Mitchell Int. Liberty</a></li>
+<li><a href='https://meaninginhistory.blogspot.com/'>Meaning of History</a></li>
+<li><a href='https://themarketswork.com/'>The MarketsWork</a></li>
+
+
+<li><a href='http://www.dailycaller.com/'>Daily Caller</a></li>
+<li><a href='https://twitter.com/donsurber'>Twitter Don Surber</a></li>
+<li><a href='https://twitter.com/johnwhuber?lang=en'>Twitter Undercover Huber</a></li>
+<li><a href='https://twitter.com/aaronjmate'>Twitter Aaron Mate</a></li>
+<li><a href='https://twitter.com/jsolomonReports'>Twitter John Solomon</a></li>
+
+<li><a href='https://news.idealmedia.com/'>Ideal Media</a></li>
+<li><a href='https://www.buzzfeed.com/news'>BuzzFeed</a></li>
+
+
+
+[
+{"href":"https://www.conservativereview.com/","title":"Conservative Review"},
+{"href":"https://gatesofvienna.net/","title":"Gates of Vienna"},
+{"href":"https://theintercept.com/","title":"The Intercept"},
+{"href":"https://thepoliticalinsider.com/","title":"Political Insider"},
+{"href":"https://www.thedailybeast.com/","title":"Daily Beast"},
+{"href":"https://www.emptywheel.net/","title":"EmptyWheel"},
+{"href":"http://turcopolier.typepad.com/sic_semper_tyrannis/","title":"Sic Semper Tyrannis"},
+{"href":"http://www.captainsquartersblog.com/mt/","title":"Captain's Quarters"},
+{"href":"http://littlegreenfootballs.com/weblog/","title":"Little Green Footballs"},
+{"href":"http://www.michellemalkin.com/","title":"Michelle Malkin"},
+{"href":"http://americanthinker.com/","title":"American Thinker"},
+{"href":"http://www.miamiherald.com/news/local/news-columns-blogs/andres-oppenheimer/","title":"Andres Oppenheimer"},
+{"href":"http://rogerlsimon.com/","title":"Roger L. Simon"},
+{"href":"http://www.hughhewitt.com/","title":"Hugh Hewitt"},
+{"href":"http://www.instapundit.com/","title":"Instapundit"},
+{"href":"http://blogs.spectator.co.uk/","title":"SpectatorUK blog"},
+{"href":"http://www.realclearpolitics.com/","title":"RealClearPolitics"},
+{"href":"https://reason.com/volokh/","title":"Volokh"},
+{"href":"https://gellerreport.com/","title":"Pamela Geller Report"},
+{"href":"http://www.crooksandliars.com/","title":"CrooksAndLiars"},
+
+{"href":"http://www.dailykos.com/","title":"Dail Kos(Left)"},
+{"href":"http://www.talkingpointsmemo.com/","title":"Talking Points Memo (L)"},
+
+{"href":"http://www.spectator.co.uk/alexmassie/","title":"Alex Massie (Spectator)"},
+
+{"href":"http://diariodeamerica.com/","title":"DiarioDeAmerica"},
+{"href":"http://www.noticias24.com/","title":"Noticias24 VZ right"},
+{"href":"https://dolartoday.com/","title":"DolarToday"},
+{"href":"https://www.lapatilla.com/","title":"La Patilla"},
+{"href":"https://www.analitica.com/","title":"Analitica"},
+{"href":"http://www.vcrisis.com/","title":"Vz VC Crisis"},
+{"href":"http://www.laht.com/","title":"Latin American Heral Tribune"},
+{"href":"http://lasarmasdecoronel.blogspot.com/","title":"Gustavo Coronel"},
+{"href":"http://daniel-venezuela.blogspot.com/","title":"Vz News & Views"},
+{"href":"http://devilsexcrement.com/","title":"Vz Devil's Excrement"},
+{"href":"http://www.infobae.com/","title":"Infobae ARG left"},
+{"href":"http://www.pagina12.com.ar/","title":"Pagina12 ARG left"},
+{"href":"http://www.urgente24.com/","title":"Urgente24 ARG left"},
+
+{"href":"https://periodicotribuna.com.ar/","title":"La Tribunade Periodistas ARG"},
+
+{"href":"http://www.lapoliticaonline.com.ar/","title":"LaPoliticaOnline ARG"},
+
+{"href":"http://no-pasaran.blogspot.com/","title":"No pasaran"},
+
+{"href":"http://barcepundit-english.blogspot.com/","title":"Barce pundit"},
+{"href":"http://unatemporadaenelinfierno.net/","title":"Temporada En El Infierno"},
+]
