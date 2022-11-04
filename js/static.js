917ab92bfd12b1c25ddd1d56bbb768a7febde30e
Adolfo Fabregat <agfabregat@hotmail.com>
2022-11-03 22:50:09 -0400

    prototypes

---
+++ js/static.js
@@ -0,0 +1,11 @@
+[
+{"href":"Nitrogen.html","title":"Nitrogen"},
+
+{"href":"http://oddsapi.azurewebsites.net/odds.html","title":"Odds"},
+{"href":"http://zjx.azurewebsites.net/zjx.html","title":"ZJX"},
+{"href":"MobileScreen.html","title":"Mobile Screen"},
+{"href":"/static/ScreenWidths.html","title":"ScreenWidths"},
+{"href":"LineOriginators.html","title":"Line Originators"},
+{"href":"STICKYHEADERSFOOTERS.html","title":"StickyHeadersFooters"}
+
+]
