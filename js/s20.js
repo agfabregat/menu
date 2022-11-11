4754d6c2b7caf0092f74d8d24447d680d610553f
Adolfo Fabregat <agfabregat@hotmail.com>
2022-11-11 17:06:34 -0500

    S20

--- js/s20.js
+++ js/s20.js
@@ -3,10 +3,12 @@
 {"href":"https://www.target.com/p/samsung-galaxy-s20-5g-128gb-rom-8gb-ram-g981-unlocked-smartphone-manufacturer-refurbished/-/A-83215709?preselect=83215710#lnk=sametab","title":"S20 Target"},
 
 {"href":"https://www.amazon.com/Samsung-Galaxy-S20-5G-Cosmic/dp/B08B8Y5R4Y/ref=sr_1_3?adgrpid=1338106293166487&hvadid=83631961466878&hvbmt=bp&hvdev=c&hvlocphy=80127&hvnetw=o&hvqmt=p&hvtargid=kwd-83632051969727%3Aloc-190&hydadcr=14907_13423935&keywords=samson+galaxy+s20&qid=1668107297&sr=8-3","title":"S20 Amazon"},
+
 {"href":"https://www.amazon.com/Samsung-Galaxy-S20-5G-Cosmic/dp/B08B8Y5R4Y/ref=sr_1_4?adgrpid=1342504269271595&hvadid=83906731256880&hvbmt=be&hvdev=c&hvlocphy=80962&hvnetw=o&hvqmt=e&hvtargid=kwd-83906878661861%3Aloc-190&hydadcr=7502_13183129&keywords=sm-g981u&qid=1668181602&sr=8-4","title":"S20 Amazon"},
 "https://www.dhgate.com/product/refurbished-original-samsung-galaxy-s20-g781u/712651238.html?f=bm|GDN|Dyn|18628599031|139812864702|628790807259|||200560|www.youtube.com|c|2|&gclid=Cj0KCQiA37KbBhDgARIsAIzce15owK5hy2sYiXdLK9AXCcSPiWdonHvcC5pSGHQZQpspnw1wuyC3MOYaArZAEALw_wcB&skuid=961835166149476392","title":"S20 DHGate"},
+
 {"href":"https://www.ebay.com/itm/185619887210?chn=ps&var=693493885290&norover=1&mkevt=1&mkrid=711-160867-114178-3&mkcid=2&mkscid=101&itemid=693493885290_185619887210&targetid=4580909049148151&device=c&mktype=&googleloc=&poi=&campaignid=422130743&mkgroupid=1236950822564187&rlsatarget=pla-4580909049148151&abcId=9300662&merchantid=51291&msclkid=e2069d78f79b158875bdfe15b80c29f8","title":"S20 Ebay"},
-{"href":
+
 {"href":"https://www.ebay.com/itm/134311547879","title":"S20 Ebay"},
 {"href":"https://www.ebay.com/itm/255593475026","title":"S20 Ebay"},
 {"href":"https://www.ebay.com/itm/254737765290","title":"S20 Ebay"},
@@ -15,10 +17,7 @@
 {"href":"https://www.ebay.com/itm/334470185625","title":"S20 Ebay"},
 {"href":"https://www.ebay.com/itm/254688120635","title":"S20 Ebay"},
 
-{"href":"https://www.ebay.com/itm/225022649272","title":"S20 Ebay"},
-{"href":"","title":""},
-{"href":"","title":""},
-{"href":"","title":""},
-{"href":"","title":""}
+{"href":"https://www.ebay.com/itm/225022649272","title":"S20 Ebay"}
+
 
 ]
