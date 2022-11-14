c3abc92a9ef74fe8b92562bb6aa4af2be6f15767
Adolfo Fabregat <agfabregat@hotmail.com>
2022-11-14 12:58:36 -0500

    First commit

--- js/static.js
+++ js/static.js
@@ -11,7 +11,9 @@
 {"href":"./static/Nitrogen.html","title":"Nitrogen"},
 {"href":"http://oddsapi.azurewebsites.net/odds.html","title":"Odds"},
 {"href":"http://zjx.azurewebsites.net/zjx.html","title":"ZJX"},
+{"href":"./static/agatefont.html","title":"AgateFont"},
 {"href":"./static/MobileScreen.html","title":"Mobile Screen"},
+{"href":"./static/deviceDetector.html","title":"DeviceDetector"},
 {"href":"./static/ScreenWidths.html","title":"ScreenWidths"},
 {"href":"./static/LineOriginators.html","title":"Line Originators"},
 {"href":"./static/STICKYHEADERSFOOTERS.html","title":"StickyHeadersFooters"}
