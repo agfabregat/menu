55803bf3dc79a8ffb9cc90a70c11d9eed7bee4ef
Adolfo Fabregat <agfabregat@hotmail.com>
2022-11-15 17:13:17 -0500

    First commit

--- js/s20.js
+++ js/s20.js
@@ -1,11 +1,11 @@
 
 [
-{"href":"https://www.target.com/p/samsung-galaxy-s20-5g-128gb-rom-8gb-ram-g981-unlocked-smartphone-manufacturer-refurbished/-/A-83215709?preselect=83215710#lnk=sametab","title":"S20 Target"},
+{"href":"https://www.target.com/p/samsung-galaxy-s20-5g-128gb-rom-8gb-ram-g981-unlocked-smartphone-manufacturer-refurbished/-/A-83215709?preselect=83215710#lnk=sametab","title":"S20 249 Target"},
 
-{"href":"https://www.amazon.com/Samsung-Galaxy-S20-5G-Cosmic/dp/B08B8Y5R4Y/ref=sr_1_3?adgrpid=1338106293166487&hvadid=83631961466878&hvbmt=bp&hvdev=c&hvlocphy=80127&hvnetw=o&hvqmt=p&hvtargid=kwd-83632051969727%3Aloc-190&hydadcr=14907_13423935&keywords=samson+galaxy+s20&qid=1668107297&sr=8-3","title":"S20 Amazon"},
+{"href":"https://www.amazon.com/Samsung-Galaxy-S20-5G-Cosmic/dp/B08B8Y5R4Y/ref=sr_1_3?adgrpid=1338106293166487&hvadid=83631961466878&hvbmt=bp&hvdev=c&hvlocphy=80127&hvnetw=o&hvqmt=p&hvtargid=kwd-83632051969727%3Aloc-190&hydadcr=14907_13423935&keywords=samson+galaxy+s20&qid=1668107297&sr=8-3","title":"S20 246 Amazon"},
 
 {"href":
-"https://www.dhgate.com/product/refurbished-original-samsung-galaxy-s20-g781u/712651238.html?f=bm|GDN|Dyn|18628599031|139812864702|628790807259|||200560|www.youtube.com|c|2|&gclid=Cj0KCQiA37KbBhDgARIsAIzce15owK5hy2sYiXdLK9AXCcSPiWdonHvcC5pSGHQZQpspnw1wuyC3MOYaArZAEALw_wcB&skuid=961835166149476392","title":"S20 DHGate"},
+"https://www.dhgate.com/product/refurbished-original-samsung-galaxy-s20-g781u/712651238.html?f=bm|GDN|Dyn|18628599031|139812864702|628790807259|||200560|www.youtube.com|c|2|&gclid=Cj0KCQiA37KbBhDgARIsAIzce15owK5hy2sYiXdLK9AXCcSPiWdonHvcC5pSGHQZQpspnw1wuyC3MOYaArZAEALw_wcB&skuid=961835166149476392","title":"S20 NEW 313 DHGate"},
 
 
 {"href":"https://www.ebay.com/itm/255593475026","title":"S20 Good 209 Ebay"},
@@ -14,10 +14,10 @@
 {"href":"https://www.ebay.com/itm/334470159283","title":"S20 Good 179 Ebay"},
 {"href":"https://www.ebay.com/itm/334470185625","title":"S20 Excellent 219 Ebay"},
 {"href":"https://www.ebay.com/itm/254688120635","title":"S20 Excellent 249 Ebay"},
-{"href":"https://www.walmart.com/ip/BLU-F91-5G-F0030UU-128GB-Dual-SIM-GSM-Unlocked-Android-Smartphone-Oceanic-Blue/261145934","title":"BLU F91 5G F0030UU Walmart"},
+{"href":"https://www.walmart.com/ip/BLU-F91-5G-F0030UU-128GB-Dual-SIM-GSM-Unlocked-Android-Smartphone-Oceanic-Blue/261145934","title":"BLU F91 5G F0030UU 229 Walmart"},
 
-{"href":"https://www.ebay.com/itm/134311547879","title":"BLU F91 5G F0030UU Ebay"},
-{"href":"https://www.amazon.com/Battery-Unlocked-AMOLED-Display-Camera/dp/B0B3B8VY4D","title":"BluN2 Amazon"}
+{"href":"https://www.ebay.com/itm/134311547879","title":"BLU F91 5G F0030UU 229 Ebay"},
+{"href":"https://www.amazon.com/Battery-Unlocked-AMOLED-Display-Camera/dp/B0B3B8VY4D","title":"BluN2 249 Amazon"}
 
 
 ]
