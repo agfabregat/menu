19e880bf1dbee86d6c4db31d51ba118931491cb5
Adolfo Fabregat <agfabregat@hotmail.com>
2022-11-19 13:05:42 -0500

    First commit

--- js/s20.js
+++ js/s20.js
@@ -2,7 +2,7 @@
 [
 {"href":"https://www.target.com/p/samsung-galaxy-s20-5g-128gb-rom-8gb-ram-g981-unlocked-smartphone-manufacturer-refurbished/-/A-83215709?preselect=83215710#lnk=sametab","title":"S20 249 Target"},
 
-{"href":"https://www.amazon.com/Samsung-Galaxy-S20-5G-Cosmic/dp/B08B8Y5R4Y/ref=sr_1_3?adgrpid=1338106293166487&hvadid=83631961466878&hvbmt=bp&hvdev=c&hvlocphy=80127&hvnetw=o&hvqmt=p&hvtargid=kwd-83632051969727%3Aloc-190&hydadcr=14907_13423935&keywords=samson+galaxy+s20&qid=1668107297&sr=8-3","title":"S20 246 Amazon"},
+{"href":"https://www.amazon.com/dp/B08B8Y5R4Y/ref=sr_1_3?adgrpid=1338106293166487&hvadid=83631961466878&hvbmt=bp&hvdev=c&hvlocphy=80127&hvnetw=o&hvqmt=p&hvtargid=kwd-83632051969727%3Aloc-190&hydadcr=14907_13423935&keywords=samson+galaxy+s20&qid=1668107297&sr=8-3","title":"S20 246 Amazon"},
 
 {"href":
 "https://www.dhgate.com/product/refurbished-original-samsung-galaxy-s20-g781u/712651238.html?f=bm|GDN|Dyn|18628599031|139812864702|628790807259|||200560|www.youtube.com|c|2|&gclid=Cj0KCQiA37KbBhDgARIsAIzce15owK5hy2sYiXdLK9AXCcSPiWdonHvcC5pSGHQZQpspnw1wuyC3MOYaArZAEALw_wcB&skuid=961835166149476392","title":"S20 NEW 313 DHGate"},
@@ -22,7 +22,11 @@
 {"href":"https://www.ebay.com/itm/175475999562","title":"BLU F91+SD 5G F0030UU 229 Ebay"},
 
 {"href":"https://www.ebay.com/itm/134311547879","title":"BLU F91 5G F0030UU 229 Ebay"},
-{"href":"https://www.amazon.com/Battery-Unlocked-AMOLED-Display-Camera/dp/B0B3B8VY4D","title":"BluN2 249 Amazon"}
+
+
+{"href":"https://www.amazon.com/dp/B0B3B8VY4D/ref=sspa_dk_detail_0","title":"BluN2 249 Amazon"},
+
+{"href":"https://www.amazon.com/dp/B0B3B8VY4D","title":"BluN2 249 Amazon"}
 
 
 ]
