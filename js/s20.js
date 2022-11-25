e900bb5ce3957600199940bb50601867ae2a7e4d
Adolfo Fabregat <agfabregat@hotmail.com>
2022-11-24 23:30:13 -0500

    First commit

--- js/s20.js
+++ js/s20.js
@@ -7,24 +7,19 @@
 {"href":
 "https://www.dhgate.com/product/refurbished-original-samsung-galaxy-s20-g781u/712651238.html?f=bm|GDN|Dyn|18628599031|139812864702|628790807259|||200560|www.youtube.com|c|2|&gclid=Cj0KCQiA37KbBhDgARIsAIzce15owK5hy2sYiXdLK9AXCcSPiWdonHvcC5pSGHQZQpspnw1wuyC3MOYaArZAEALw_wcB&skuid=961835166149476392","title":"S20 NEW 313 DHGate"},
 
-
 {"href":"https://www.ebay.com/itm/255593475026","title":"S20 Good 209 Ebay"},
 {"href":"https://www.ebay.com/itm/254737765290","title":"S20 VGood 209 Ebay"},
 {"href":"https://www.ebay.com/itm/265889854424","title":"S20 Good 229 Ebay"},
-{"href":"https://www.ebay.com/itm/334470159283","title":"S20 Good 179 Ebay"},
+{"href":"https://www.ebay.com/itm/194299543054","title":"S20 Good 189 Ebay"},
 {"href":"https://www.ebay.com/itm/334470185625","title":"S20 Excellent 219 Ebay"},
 {"href":"https://www.ebay.com/itm/254688120635","title":"S20 Excellent 249 Ebay"},
 
 {"href":"https://www.amazon.com/dp/B09WJM9XVW/ref=sr_1_1_sspa","title":"BLU F91 5G F0030UU 229 Amazon"},
 {"href":"https://www.walmart.com/ip/BLU-F91-5G-F0030UU-128GB-Dual-SIM-GSM-Unlocked-Android-Smartphone-Oceanic-Blue/261145934","title":"BLU F91 5G F0030UU 229 Walmart"},
 
-
 {"href":"https://www.ebay.com/itm/175475999562","title":"BLU F91+SD 5G F0030UU 229 Ebay"},
 
 {"href":"https://www.ebay.com/itm/134311547879","title":"BLU F91 5G F0030UU 229 Ebay"},
-
-
-{"href":"https://www.amazon.com/dp/B0B3B8VY4D/ref=sspa_dk_detail_0","title":"BluN2 249 Amazon"},
 
 {"href":"https://www.amazon.com/dp/B0B3B8VY4D","title":"BluN2 249 Amazon"}
 
