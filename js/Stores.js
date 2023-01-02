5750d2bf679b8565ce369965537c116b1b1d9373
Adolfo Fabregat <agfabregat@hotmail.com>
2023-01-02 12:54:02 -0500

    Mon 01/02/2023 12:54:02.07

--- js/Stores.js
+++ js/Stores.js
@@ -1,37 +1,10 @@
 [
-{"href":"http://www.slickdeals.net/","title":"SlickDeals"},
-{"href":"https://www.511tactical.com/","title":"511 Tactical"},
-{"href":"https://www.outerknown.com/","title":"OuterKnown"},
-{"href":"http://www.passwird.com/","title":"PassWird"},
-{"href":"http://www.jcrew.com/","title":"JCrew"},
-{"href":"http://www.oldnavy.com/","title":"OldNavy"},
-{"href":"http://www.orvis.com/","title":"Orvis"},
-{"href":"http://www.territoryahead.com/","title":"TerritoryAhead"},
-{"href":"http://www.llbean.com/","title":"LLBean"},
-{"href":"http://www.landsend.com/","title":"LandsEnd"},
-{"href":"http://www.eddiebauer.com/","title":"EddieBauer"},
-{"href":"http://www.eddiebaueroutlet.com/","title":"EddieBauerOutlet"},
-{"href":"http:www.//josbank.com/","title":"JosBank"},
-{"href":"http://www.boardtactics.com/","title":"BoardTactics"},
-
-{"href":"http://www.luckybrandjeans.com/","title":"LuckyBrand"},
-
-{"href":"http://www.cutterbuck.com/","title":"Cutter&Buck"},
-
-{"href":"http://www.polo.com/","title":"Polo"},
-
-{"href":"http://www.rei-outlet.com/","title":"ReiOutlet"},
-{"href":"http://www.ghurka.com/","title":"Ghurka"},
-{"href":"http://www.coach.com/","title":"Coach"},
-{"href":"http://www.tshipley.com/","title":"TShipley"},
-{"href":"http://www.dooney.com/","title":"Dooney"},
-{"href":"http://www.colehaan.com/","title":"ColeHaan"},
-{"href":"http://www.shoebuy.com/","title":"ShoeBuy"},
-{"href":"http://www.zappos.com/","title":"Zappos"},
-{"href":"https://zapateriaguido.com/","title":"Guido"},
-{"href":"https://www.bedbathandbeyond.com/","title":"BedBath&Beyond"},
-{"href":"https://www.williams-sonoma.com/?cm_type=gnav","title":"Williams&Sonoma"},
-{"href":"https://www.crateandbarrel.com/","title":"Crate&Barrel"}
-
+{"href":"https://www.rocketlaunch.live/","title":"RocketLaunch Live"},
+{"href":"https://everydayastronaut.com/","title":"Everyday Astronaut"},
+{"href":"https://www.spacelaunchschedule.com/","title":"SpaceLaunchSchedule"},
+{"href":"https://travelspock.com/best-places-to-watch-a-rocket-launch-at-cape-canaveral-2022/#:~:text=Best%20Places%20To%20Watch%20A%20Rocket%20Launch%20At,crown%20of%20Florida%E2%80%99s%20Space%20Coast%20...%20More%20items","title":"Best Places to Watch"},
+{"href":"https://www.nasa.gov/centers/kennedy/launchingrockets/viewing.html","title":"Cape Launches up colse"},
+{"href":"https://nextspaceflight.com/","title":"NextSpaceFlight"},
+{"href":"https://flightclub.io/build/choose-template","title":"Path Simulation 3D"}
 
 ]
