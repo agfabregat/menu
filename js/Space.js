2c879c9f9b024041bbff613a6d8d21a804f75935
Adolfo Fabregat <agfabregat@hotmail.com>
2023-01-02 14:28:05 -0500

    Mon 01/02/2023 14:28:05.09

---
+++ js/Space.js
@@ -0,0 +1,12 @@
+[
+{"href":"https://www.rocketlaunch.live/","title":"RocketLaunch Live"},
+{"href":"https://everydayastronaut.com/","title":"Everyday Astronaut"},
+{"href":"https://www.spacelaunchschedule.com/","title":"SpaceLaunchSchedule"},
+{"href":"https://travelspock.com/best-places-to-watch-a-rocket-launch-at-cape-canaveral-2022/#:~:text=Best%20Places%20To%20Watch%20A%20Rocket%20Launch%20At,crown%20of%20Florida%E2%80%99s%20Space%20Coast%20...%20More%20items","title":"Best Places to Watch"},
+{"href":"https://www.nasa.gov/centers/kennedy/launchingrockets/viewing.html","title":"Cape Launches up colse"},
+{"href":"https://nextspaceflight.com/","title":"NextSpaceFlight"},
+{"href":"https://flightclub.io/build/choose-template","title":"Path Simulation 3D"}
+
+
+
+]
