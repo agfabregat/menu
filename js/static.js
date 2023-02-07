5b058971aa86a6b8da933e817669c6073478d7a3
Adolfo Fabregat <agfabregat@hotmail.com>
2023-02-07 12:39:48 -0500

    Tue 02/07/2023 12:39:48.28

--- js/static.js
+++ js/static.js
@@ -7,7 +7,7 @@
 {"href":"./static/EncryptStellar/nacl-messageEncyption.html","title":"NaclMssgEncrypt"},
 {"href":"./static/EncryptStellar/nacl-pkMsgEncryption.html","title":"pkNaclMssgEncrypt"},
 {"href":"./static/EncryptStellar/stellar.html","title":"Stellar"},
-
+{"href":"./static/fonts.html","title":"Fonts"},
 {"href":"./static/Nitrogen.html","title":"Nitrogen"},
 {"href":"http://oddsapi.azurewebsites.net/odds.html","title":"Odds"},
 {"href":"http://zjx.azurewebsites.net/zjx.html","title":"ZJX"},
