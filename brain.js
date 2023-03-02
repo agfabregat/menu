239d3904f7d15c317256d5d8b0c2195e3c9fca5a
Adolfo Fabregat <agfabregat@hotmail.com>
2023-03-02 00:02:19 -0500

    Thu 03/02/2023  0:02:19.26

---
+++ brain.js
@@ -0,0 +1,11 @@
+[
+    {"href":"https://www.chess.com/daily-chess-puzzle","title":"Daily Chess"},
+    {"href":"https://www.proprofsgames.com/sliding-block-puzzle/","title":"Sliding Blocks"},
+    {"href":"https://zone.msn.com/gameplayer/gameplayerHTML.aspx?game=2048","title":"Game 2048 match tiles"},
+    {"href":"https://absolutist.com/online/fifteen/","title":"Arrange tiles to match picture"},
+    {"href":"https://absolutist.com/online/slidey-block-puzzle/","title":"Slidy Block"},
+    {"href":"https://slidingtiles.com/en/puzzle/play/art/25367-3x3-puzzle#3x3","title":"3x3 Puzzle"}
+  
+    
+    
+    ]
