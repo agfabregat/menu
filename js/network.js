6b2603bdbdd33ffcb8442928001f0c26e4ba0cca
Adolfo Fabregat <agfabregat@hotmail.com>
2023-03-09 20:00:03 -0500

    Thu 03/09/2023 20:00:03.34

---
+++ js/network.js
@@ -0,0 +1,11 @@
+
+[
+{"href":"https://speed.measurementlab.net/#/","title":"Speed Test by Measurement Lab"},
+{"href":"https://browserleaks.com/ip","title":"Browserleaks"},
+{"href":"https://www.dnsleaktest.com/","title":"DNS Leaktest 1"},
+{"href":"https://dnsleak.com/","title":"DNS Leaktest 2"}
+
+
+
+
+]
