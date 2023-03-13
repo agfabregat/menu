9729275dda4a38533daf3dd9a7db4e13b4c00b23
Adolfo Fabregat <agfabregat@hotmail.com>
2023-03-12 23:55:38 -0400

    Sun 03/12/2023 23:55:38.12

--- js/latest.js
+++ js/latest.js
@@ -2,14 +2,35 @@
     {"href":"https://www.dailymail.co.uk/ushome/index.html","title":"Daily Mail US"},
     {"href":"https://www.msn.com/en-us/news","title":"MSN News"},
     {"href":"https://www.reuters.com/","title":"Reuters"},
-    {"href":"https://twitter.com/AFP","title":"AFP"},
-    {"href":"https://twitter.com/cnni","title":"CNNI"},
-    {"href":"https://twitter.com/WSJ","title":"WSJ"},
+    {"href":"https://www.afp.com/en","title":"AFP"},
+    {"href":"https://edition.cnn.com/","title":"CNN"},
+    {"href":"https://www.wsj.com/","title":"WSJ"},
 
-    {"href":"https://twitter.com/TheEconomist","title":"The Economist"},
-    {"href":"https://twitter.com/FinancialTimes","title":"Financial Times"},
-    {"href":"https://twitter.com/business","title":"Bloomberg"},
-    {"href":"https://twitter.com/NewYorker","title":"The New Yorker"},
-    {"href":"https://twitter.com/villagevoice","title":"Village Voice"}
+    {"href":"https://www.economist.com/","title":"The Economist"},
+    {"href":"https://www.ft.com/world","title":"Financial Times"},
+
+    {"href":"https://www.aljazeera.com/","title":"AlJazeera"},
+    {"href":"https://www.bloomberg.com/","title":"Bloomberg"},
+    {"href":"https://news.sky.com/","title":"Sky"},
+    {"href":"https://www.bbc.com/news","title":"BBC"},
+    {"href":"https://esradio.libertaddigital.com/c.php?op=radio.search&program_id=20","title":"Es la Mañana de Federico"},
+    {"href":"https://citizenfreepress.com/","title":"CitizenFreePress"},
+    {"href":"https://freerepublic.com/tag/news-forum/index","title":"FreeRepublic"},
+
+    {"href":"https://spectator.org/","title":"American Spectator"},
+    {"href":"https://newrepublic.com/","title":"The New Republic"},
+    {"href":"https://www.salon.com/","title":"Salon"},
+    {"href":"https://www.theatlantic.com/","title":"The Atlantic"},
+    {"href":"https://www.thenation.com/","title":"The Nation"},
+    {"href":"https://prospect.org/","title":"The American Prospect"},
+    {"href":"https://www.frontpagemag.com/","title":"Frontpage Magazine"},
+    {"href":"https://www.takimag.com/","title":"Taki Mag"},
+    {"href":"https://www.rollingstone.com/","title":"Rolling Stone"},
+    {"href":"https://www.vanityfair.com/","title":"Vanity Fair"},
+    {"href":"https://www.esquire.com/","title":"Esquire"},
+
+    
+    {"href":"https://www.newyorker.com/","title":"The New Yorker"},
+    {"href":"https://www.villagevoice.com/","title":"Village Voice"}
   
 ]
