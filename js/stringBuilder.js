17581fc563bf253cd3271c763f80208f7f8854c1
Adolfo Fabregat <agfabregat@hotmail.com>
2023-05-14 22:53:33 -0400

    Sun 05/14/2023 22:53:33.65

--- js/stringBuilder.js
+++ js/stringBuilder.js
@@ -16,3 +16,15 @@ function StringBuilder(value) {
 	StringBuilder.prototype.toString = function () {
 		return this.strings.join("");
 	}
+
+	//var sb = new StringBuilder();
+	//for (let i of jsondata) 
+	//{ 
+	//	var t = i.title;
+	//	var h = i.href;
+	//	sb.append(`<div><a target="_blank" href="${i.href}">${i.title}</a></div>`);
+	//	//sb.append(`<div><span>${i.title}</span><span>${i.href}</span></div>`);
+	//}
+	//let content = document.getElementById("content");
+	//content.innerHTML=sb.toString(); 
+	//sb.clear();
