785b68d757a3f19620989daff7715c4245445cce
Adolfo Fabregat <agfabregat@hotmail.com>
2024-12-09 15:20:27 -0500

    Mon 12/09/2024 15:20:27.29

--- js/radioTv.js
+++ js/radioTv.js
@@ -14,6 +14,9 @@
 {"href":"https://www.rsbnetwork.com/category/video/","title":"RSBN"},
 {"href":"https://gettr.com/","title":"GTTR gb PE#5"},
 
+
+{"href":"http://atdhes.eu/","title":"Atdhes sports"},
+{"href":"http://livetv.sx/enx/","title":"LiveTv (all live sports)"},
 {"href":"https://live.sport365.stream/tennis","title":"Sport365 Tennis"},
 
 {"href":"https://utakmice.net/tenis/","title":"TENNIS UTAKMICE"},
@@ -31,30 +34,18 @@
 {"href":"https://www.fis-ski.com/DB/alpine-skiing/","title":"Skiing Results"},
 
 {"href":"https://esradio.libertaddigital.com/es-la-manana-de-federico/","title":"Es la Mañana de Federico"},
-{"href":"https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkLm5hc2hvd25vdGVzLmNvbS9yc3MueG1s","title":"No Agenda"},
-{"href":"https://podcasts.google.com/feed/aHR0cHM6Ly9wb3N0LmZ1dHVyaW1lZGlhLmNvbS93b3IvcGxheWxpc3QvcnNzLzIueG1s","title":"Mark Simone"},
-{"href":"https://podcasts.google.com/feed/aHR0cHM6Ly9hc2thc3BhY2VtYW4ubGlic3luLmNvbS9yc3M","title":"Ask A Spaceman"},
-{"href":"https://podcasts.google.com/feed/aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvNTYzNDc5My9lcGlzb2Rlcy9mZWVk","title":"DotNet Rocks!"},
-{"href":"https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy50d2l0LnR2L3NuLnhtbA","title":"Security Now"},
-{"href":"https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy8xMjhkMDcyYy9wb2RjYXN0L3Jzcw?sa=X&ved=0CA4Q27cFahcKEwiIxIrfl_r9AhUAAAAAHQAAAAAQAQ","title":"Scott Adams"},
 
-{"href":"http://streamuruguay5.blogspot.com/","title":"StreamUruguay5"},
-{"href":"http://www.rojadirecta.me/","title":"RojaDirecta ME"},
+
 {"href":"http://rojadirecta.su/","title":"RojaDirecta SU"},
-{"href":"http://www.tarjetaroja.eu/","title":"TarjetaRoja"},
+
 
 {"href":"http://www.rtve.es/noticias/directo-la-1/","title":"RTVE.ES San Fermin"},
-{"href":"http://www.radios.co.ve/rcr-caracas/#","title":"RCR Vz"},
-{"href":"http://www.radios.co.ve/actualidad-caracas/#","title":"Union Radio Caracas Vz"},
-{"href":"http://streema.com/radios/play/93215","title":"Union Radio Barqui Vz"},
-{"href":"http://streema.com/radios/play/38123","title":"Union Radio P.Ordaz Vz"},
 
-{"href":"https://www.hispantv.com/directo","title":"Hispantv"},
-{"href":"https://www.cronoco.com/2014/03/vtv-en-vivo-vea-venezolana-de.html","title":"VTV Vz"},
-{"href":"https://www.youtube.com/watch?v=TQrzPbL9Dh4&feature=youtu.be","title":"Telesur"},
+
+
 {"href":"https://www.raiplay.it/dirette/rainews24","title":"RAI"},
 
-{"href":"http://cricfree.ws/update/bt1","title":"BT 1"},
+
 {"href":"https://www.rt.com/on-air/","title":"Russia Today"},
 {"href":"https://www.dw.com/en/media-center/live-tv/s-100825?channel=1","title":"DWTV (Deutsche Welle)"},
 
@@ -72,11 +63,6 @@
 
 {"href":"https://ssl.ustreamix.com/","title":"UstreamMix"},
 {"href":"https://tvplayer.com/watch","title":"TV Player (uk ip hotmail PE#5)"},
-
-{"href":"http://music-tv.in/?tab=usa%20tv&live=espn%202%20usa%20live","title":"Music Tv USA Ch"},
-{"href":"http://atdhes.eu/","title":"Atdhes"},
-{"href":"http://livetv.sx/enx/","title":"LiveTv (all live sports)"},
-
 
 
 {"href":"http://www.freeintertv.com/view/id-3324/USA-1-0-1ertv.com/view/id-2566","title":"Free internet TV"},
