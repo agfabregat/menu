040fce84716f6d7ba7a5fd0446567b4e3b3f14fe
Adolfo Fabregat <agfabregat@hotmail.com>
2025-01-02 19:06:49 -0500

    Thu 01/02/2025 19:06:49.56

--- js/prototypes.js
+++ js/prototypes.js
@@ -1,58 +1,158 @@
 [
-{"href":"https://www.flashscore.com/tennis/wta-singles/","title":"WTA Schedule"},
-{"href":"https://playarea.azurewebsites.net/papers.html","title":"Morning Papers"},
-{"href":"https://www.the-sun.com/entertainment/","title":"The Sun Entertainment"},
-{"href":"https://www.investopedia.com/what-to-expect-for-the-markets-next-week-4584772","title":"Investopedia This Week"},
-
-{"href":"http://adolfofabregat-001-site2.htempurl.com/tennisOdds.html","title":"TennisOdds"},
-{"href":"https://aes.uunet.io/","title":"Test AES"},
-{"href":"https://machine.uunet.io","title":"machine.uunet.io"},
-
-{"href":"https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=29016&maxPrice=17000&inventorySearchWidgetType=AUTO&sortDir=ASC&sourceContext=rtb_house&distance=50&sortType=BEST_MATCH&entitySelectingHelper.selectedEntity=d2222","title":"MINI PACE (GURUS)"},
-{"href":"https://www.cars.com/shopping/mini-paceman-2014-cooper_s_all4/?utm_source=bing&utm_medium=cpc&utm_campaign_id=376544604&utm_trusted=TRUE&network=o&ag_id=1202866315253468&loc_physical_ms=80127&loc_interest_ms=&aff=acqgeosem46&KNC=acqgeosem46&msclkid=c3053d103f9210ed784c5b8d80b2bb99&utm_campaign=DSA%20-%20US%2Fen%20-%20NB%20-%20Make%3AAll&utm_term=%2Fmini-%20%2Fshopping%2F&utm_content=URL%20contains%20other%20makes%20%7C%20Make%3AOTHER%20MAKES","title":"MINI PACE (CARS)"},
-
-{"href":"https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=29016&maxPrice=17000&inventorySearchWidgetType=AUTO&sortDir=ASC&sourceContext=rtb_house&distance=50&sortType=BEST_MATCH&entitySelectingHelper.selectedEntity=m3","title":"GURUS BMW"},
-{"href":"https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=29016&maxPrice=17000&inventorySearchWidgetType=AUTO&sortDir=ASC&sourceContext=rtb_house&distance=50&sortType=BEST_MATCH&entitySelectingHelper.selectedEntity=m35","title":"GURUS LAND ROVER"},
-{"href":"https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=29016&maxPrice=17000&inventorySearchWidgetType=AUTO&sortDir=ASC&sourceContext=rtb_house&distance=50&sortType=BEST_MATCH&entitySelectingHelper.selectedEntity=m43","title":"GURUS MERCEDES"},
-{"href":"","title":""},
-{"href":"","title":""},
-{"href":"","title":""},
-{"href":"","title":""},
-
-
-
-{"href":"https://property.spatialest.com/sc/richland/#/property/R17805-01-02", "title":"RimerPond Title"},
-{"href":"https://www.richlandmaps.com/apps/geoinfo/?lat=34.19901&lon=-80.95641&zoom=17&taxid=R17805-01-02", "title":"RimerPond Title2"},
-{"href":"https://binance.us/en/trade/pro/XNO_USD","title":"XNO USD"},
-{"href":"https://binance.us/en/trade/pro/ATOM_USD","title":"ATOM USD"},
-{"href":"https://binance.us/en/trade/pro/XLM_USD","title":"XLM USD"},
-{"href":"https://binance.us/en/trade/pro/BNB_USD","title":"BNB USD"},
-
-{"href":"https://archive.is/","title":"Archive is"},
-{"href":"https://agfservicecorsproxy20220326232156.azurewebsites.net/htmlget.html","title":"HtmlGet"},
-
-{"href":"https://www.livesporttv.com/","title":"SportsOnTv"},
-{"href":"https://www.fis-ski.com/DB/general/live.html","title":"Alpine Skiing"},
-
-{"href":"https://popminute.com/tv-tonight/","title":"TV Primetime"},
-
-{"href":"https://noises.online/player.php?g=ha5","title":"MyNoise"},
-
-{"href":"http://agfabregat-001-site2.atempurl.com","title":"DNS test"},
-
-{"href":"https://www.bing.com/search?q=persons+born+on+this+day&filters=tnTID%3a%22B5D56CB0-FF9A-41c8-BFA5-8C3125CC4D34%22+tnVersion%3a%225034561%22+Segment%3a%22popularnow.carousel%22+tnCol%3a%223%22+tnOrder%3a%22f4918dff-af58-431d-a412-e5a42c1a11d8%22&FORM=TOBRR0&ecount=50","title":"Trending Now"},
-{"href":"http://clexplorer.azurewebsites.net/clexplorer.html","title":"clExplorer"},
-{"href":"http://agfabregat-001-site1.atempurl.com/","title":"Awesome Airfare Helper"},
-{"href":"http://agfabregat-001-site3.atempurl.com/rss-reader.html#","title":"rssReader"},
-
-{"href":"http://agfabregat-001-site3.atempurl.com/content/scitech.html","title":"Sci Tech"},
-
-
-{"href":"https://taskandpurpose.com/","title":"Task & Purpose"},
-{"href":"http://www.thedrive.com/","title":"The Drive"},
-
-{"href":"https://news.usni.org/","title":"US Naval Inst."},
-{"href":"https://nationalinterest.org/","title":"National Interest"},
-{"href":"https://www.strategypage.com/default.aspx","title":"Strategy Page"}
-
+  {
+    "href": "https://www.flashscore.com/tennis/wta-singles/",
+    "title": "WTA Schedule"
+  },
+  {
+    "href": "https://playarea.azurewebsites.net/papers.html",
+    "title": "Morning Papers"
+  },
+  {
+    "href": "https://www.the-sun.com/entertainment/",
+    "title": "The Sun Entertainment"
+  },
+  {
+    "href": "https://www.investopedia.com/what-to-expect-for-the-markets-next-week-4584772",
+    "title": "Investopedia This Week"
+  },
+  {
+    "href": "http://adolfofabregat-001-site2.htempurl.com/tennisOdds.html",
+    "title": "TennisOdds"
+  },
+  {
+    "href": "https://aes.uunet.io/",
+    "title": "Test AES"
+  },
+  {
+    "href": "https://machine.uunet.io",
+    "title": "machine.uunet.io"
+  },
+  {
+    "href": "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=29016&maxPrice=17000&inventorySearchWidgetType=AUTO&sortDir=ASC&sourceContext=rtb_house&distance=50&sortType=BEST_MATCH&entitySelectingHelper.selectedEntity=d2222",
+    "title": "MINI PACE (GURUS)"
+  },
+  {
+    "href": "https://www.cars.com/shopping/mini-paceman-2014-cooper_s_all4/?utm_source=bing&utm_medium=cpc&utm_campaign_id=376544604&utm_trusted=TRUE&network=o&ag_id=1202866315253468&loc_physical_ms=80127&loc_interest_ms=&aff=acqgeosem46&KNC=acqgeosem46&msclkid=c3053d103f9210ed784c5b8d80b2bb99&utm_campaign=DSA%20-%20US%2Fen%20-%20NB%20-%20Make%3AAll&utm_term=%2Fmini-%20%2Fshopping%2F&utm_content=URL%20contains%20other%20makes%20%7C%20Make%3AOTHER%20MAKES",
+    "title": "MINI PACE (CARS)"
+  },
+  {
+    "href": "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=29016&maxPrice=17000&inventorySearchWidgetType=AUTO&sortDir=ASC&sourceContext=rtb_house&distance=50&sortType=BEST_MATCH&entitySelectingHelper.selectedEntity=m3",
+    "title": "GURUS BMW"
+  },
+  {
+    "href": "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=29016&maxPrice=17000&inventorySearchWidgetType=AUTO&sortDir=ASC&sourceContext=rtb_house&distance=50&sortType=BEST_MATCH&entitySelectingHelper.selectedEntity=m35",
+    "title": "GURUS LAND ROVER"
+  },
+  {
+    "href": "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=29016&maxPrice=17000&inventorySearchWidgetType=AUTO&sortDir=ASC&sourceContext=rtb_house&distance=50&sortType=BEST_MATCH&entitySelectingHelper.selectedEntity=m43",
+    "title": "GURUS MERCEDES"
+  },
+  {
+    "href": "",
+    "title": ""
+  },
+  {
+    "href": "",
+    "title": ""
+  },
+  {
+    "href": "",
+    "title": ""
+  },
+  {
+    "href": "",
+    "title": ""
+  },
+  {
+    "href": "https://property.spatialest.com/sc/richland/#/property/R17805-01-02",
+    "title": "RimerPond Title"
+  },
+  {
+    "href": "https://www.richlandmaps.com/apps/geoinfo/?lat=34.19901&lon=-80.95641&zoom=17&taxid=R17805-01-02",
+    "title": "RimerPond Title2"
+  },
+  {
+    "href": "https://binance.us/en/trade/pro/XNO_USD",
+    "title": "XNO USD"
+  },
+  {
+    "href": "https://binance.us/en/trade/pro/ATOM_USD",
+    "title": "ATOM USD"
+  },
+  {
+    "href": "https://binance.us/en/trade/pro/XLM_USD",
+    "title": "XLM USD"
+  },
+  {
+    "href": "https://binance.us/en/trade/pro/BNB_USD",
+    "title": "BNB USD"
+  },
+  {
+    "href": "https://archive.is/",
+    "title": "Archive is"
+  },
+  {
+    "href": "https://agfservicecorsproxy20220326232156.azurewebsites.net/htmlget.html",
+    "title": "HtmlGet"
+  },
+  {
+    "href": "https://www.livesporttv.com/",
+    "title": "SportsOnTv"
+  },
+  {
+    "href": "https://www.fis-ski.com/DB/general/live.html",
+    "title": "Alpine Skiing"
+  },
+  {
+    "href": "https://popminute.com/tv-tonight/",
+    "title": "TV Primetime"
+  },
+  {
+    "href": "https://noises.online/player.php?g=ha5",
+    "title": "MyNoise"
+  },
+  {
+    "href": "http://agfabregat-001-site2.atempurl.com",
+    "title": "DNS test"
+  },
+  {
+    "href": "https://www.bing.com/search?q=persons+born+on+this+day&filters=tnTID%3a%22B5D56CB0-FF9A-41c8-BFA5-8C3125CC4D34%22+tnVersion%3a%225034561%22+Segment%3a%22popularnow.carousel%22+tnCol%3a%223%22+tnOrder%3a%22f4918dff-af58-431d-a412-e5a42c1a11d8%22&FORM=TOBRR0&ecount=50",
+    "title": "Trending Now"
+  },
+  {
+    "href": "http://clexplorer.azurewebsites.net/clexplorer.html",
+    "title": "clExplorer"
+  },
+  {
+    "href": "http://agfabregat-001-site1.atempurl.com/",
+    "title": "Awesome Airfare Helper"
+  },
+  {
+    "href": "http://agfabregat-001-site3.atempurl.com/rss-reader.html#",
+    "title": "rssReader"
+  },
+  {
+    "href": "http://agfabregat-001-site3.atempurl.com/content/scitech.html",
+    "title": "Sci Tech"
+  },
+  {
+    "href": "https://taskandpurpose.com/",
+    "title": "Task & Purpose"
+  },
+  {
+    "href": "http://www.thedrive.com/",
+    "title": "The Drive"
+  },
+  {
+    "href": "https://news.usni.org/",
+    "title": "US Naval Inst."
+  },
+  {
+    "href": "https://nationalinterest.org/",
+    "title": "National Interest"
+  },
+  {
+    "href": "https://www.strategypage.com/default.aspx",
+    "title": "Strategy Page"
+  }
 ]
